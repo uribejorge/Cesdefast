c01f337d6bd2ebbfff43d12c9edbd427228ea028
AlejaGQ <aygonzalezqu@cesde.net>
2024-06-04 18:36:44 -0500

    Se añade array con objetos

---
+++ model/ModelUsuario.js
@@ -0,0 +1,12 @@
+let usuarios = [
+  {
+    usuario: "Administrativo",
+    contrasena: "1234",
+    correo: "correolj@gmail.com",
+  },
+  {
+    usuario: "Estudiante",
+    contrasena: "1234",
+    correo: "correonuevo@hotmail.com",
+  },
+];
